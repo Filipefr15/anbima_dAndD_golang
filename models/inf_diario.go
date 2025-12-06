d26d64e116454556705b504154bbcb6f40ad8331
Filipe Rodrigues Ferreira - FATEC - Cuiaba/MT <00506769@alunosenai.mt>
2025-12-06 14:24:14 -0400

    feat(models - inf_diario): struct inf_diario

---
+++ models/inf_diario.go
@@ -0,0 +1,17 @@
+package models
+
+//go get github.com/shopspring/decimal
+import "github.com/shopspring/decimal"
+
+type inf_diario struct {
+	TpFundoClasse    *string
+	CnpjFundoClasse  *string
+	IdSubclasse      *string
+	DtComptc         *string
+	VlTotal          *decimal.Decimal
+	VlQuota          *decimal.Decimal
+	VlPatrimLiq      *decimal.Decimal
+	CaptcDia         *decimal.Decimal
+	ResgDia          *decimal.Decimal
+	NrCotst          *decimal.Decimal
+}
